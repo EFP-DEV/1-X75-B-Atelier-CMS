1f84cd3666ea6e643760ad07e26b461bc609f036
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-04-08 14:15:57 +0200

    Implement product listing on home page with database integration

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,58 @@
 <div class="container px-4 px-lg-5 mt-5">
     <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
         <?php
 
+        require_once 'app/config/db.php';
+
+        $stmt = $pdo->query('SELECT id, name, image, rating, price_min, price_max FROM products');
+
+        while ($product = $stmt->fetch()) {
+        ?>
+            <div class="col mb-5">
+                <div class="card h-100">
+                    <!-- Product image-->
+                    <img class="card-img-top" src="<?php echo $product['image']; ?>" alt="..." />
+                    <!-- Product details-->
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder"><?php echo $product['name']; ?></h5>
+                            <?php
+                            if ($product['rating'] > 0) {
+                            ?>
+                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                    <?php
+                                    for ($i = 0; $i < $product['rating']; ++$i) {
+                                        echo '<div class="bi-star-fill"></div>';
+                                    }
+                                    ?>
+                                </div>
+                            <?php
+                            }
+                            ?>
+
+                            <?php
+             
+                            if ($product['price_min'] && $product['price_max']) {
+                                echo '$40.00 - $80.00';
+                            } elseif (true) {
+                                echo '<span class="text-muted text-decoration-line-through">$20.00</span>$18.00';
+                            } else {
+                                echo '$40.00';
+                            }
+                            ?>
+
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
         ?>
 
         <div class="col mb-5">
